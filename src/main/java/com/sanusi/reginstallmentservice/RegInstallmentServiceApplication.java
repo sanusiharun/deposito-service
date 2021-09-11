2ef08bd76c4d1f3bcc5a356c96563d671a749046
sanusi <sanusi@bankmandiri.co.id>
2021-09-11 19:21:11 +0700

    first commit

---
+++ src/main/java/com/sanusi/reginstallmentservice/RegInstallmentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sanusi.reginstallmentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegInstallmentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegInstallmentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sanusi/reginstallmentservice/RegInstallmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sanusi.reginstallmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegInstallmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
